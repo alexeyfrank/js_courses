03161cc8f4287297f89a19fa3bab674e93691023
alekseyleshko <alekseyleshko@gmail.com>
2013-11-12 16:28:33 +0400

    Add mainDiagonalCheck

--- lesson1/homework/js/game.js
+++ lesson1/homework/js/game.js
@@ -66,7 +66,8 @@ function verificationConditionsVictory(cell) {
 }
 
 function playerWins(rowIndex, columnIndex) {
-  return horizontalCheck(rowIndex) || verticalCheck(columnIndex);
+  return horizontalCheck(rowIndex) || verticalCheck(columnIndex)
+    || diagonalsCheck();
 }
 
 function horizontalCheck(rowIndex) {
@@ -89,6 +90,31 @@ function verticalCheck(columnIndex) {
     }
   }
   return true;
+}
+
+function diagonalsCheck() {
+  return mainDiagonalCheck();
+}
+
+function mainDiagonalCheck() {
+  var mainDiagonal = getMainDiagonal();
+  var sum = 0;
+  for (var cellId in mainDiagonal) {
+    var cell = document.getElementById(mainDiagonal[cellId]);
+    if (cell.innerHTML == currentCharacter) {
+      sum++;
+    }
+  }
+  return sum == mainDiagonal.length? true : false;
+}
+
+var cellPrefix = "cell_"
+function getMainDiagonal() {
+  var arr = [];
+  for (var rowId = rowStartVal; rowId < rowSize; rowId++) {
+    arr.push(cellPrefix + rowId + "_" + rowId);
+  }
+  return arr;
 }
 
 function transitionPprogress() {
